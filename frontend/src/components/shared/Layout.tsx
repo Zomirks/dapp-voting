4a5876c40b585898484a91117110481b7e75394c
Cyril Fischer <github@cyril-fischer.fr>
2025-11-10 22:31:07 +0100

    add: RainbowKit & Wagmi

---
+++ frontend/src/components/shared/Layout.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex flex-col h-screen">
+            <Header />
+            <main className="flex-1 p-5">{children}</main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
