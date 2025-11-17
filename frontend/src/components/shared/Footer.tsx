59b0a58a632a784d19e88fffa2cea54e0253ab93
Cyril Fischer <github@cyril-fischer.fr>
2025-11-17 23:03:47 +0100

    edit: Footer UI

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
-        <footer className="text-center p-5">
-            All rights reserved &copy; Zomirks {new Date().getFullYear()}
+        <footer className="border-t mt-16">
+            <div className="container mx-auto px-4 py-6">
+                <p className="text-rainbowkit text-center text-sm">
+                    All rights reserved &copy; Zomirks {new Date().getFullYear()}
+                </p>
+            </div>
         </footer>
     )
 }
